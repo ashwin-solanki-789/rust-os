4bb648717b4b470840ece238e30a4659cd17a06f
Ashwin Solanki <solankiashwin789@gmail.com>
2024-12-18 21:58:46 +0530

    VGA Writer added for ASCII Chars

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,18 @@
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 // Below function is called on Panic
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_something();
 
     loop {}
 }
